06d82f258dbde4c4a67a8432b3dc79daaf954dcd
Adem Budak <admbudk@gmail.com>
2020-11-29 02:28:38 +0300

    Added: basename

---
+++ basename.cpp
@@ -0,0 +1,13 @@
+#include <filesystem>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  using namespace std;
+  using namespace std::filesystem;
+
+  if (argc > 1) {
+    std::cout << path(argv[1]).filename().string() << '\n';
+  }
+  return EXIT_SUCCESS;
+}
+
