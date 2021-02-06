e54b7d8c9f800f183ff9c6df25c8ceaf73db999b
Adem Budak <admbudk@gmail.com>
2021-02-06 19:20:44 +0300

    Create file if not exist.

--- touch.cpp
+++ touch.cpp
@@ -1,13 +1,19 @@
 #include <filesystem>
+#include <fstream>
 
-int main(int argc, char *args[]) {
+int main(int argc, char *argv[]) {
+  using namespace std;
+  using namespace std::filesystem;
+
   if (argc == 2) {
-    using namespace std;
-    using namespace std::filesystem;
-
-    error_code ec;
-    last_write_time(args[1], file_time_type::clock::now(), ec);
-    return ec.value();
+    if (!exists(argv[1])) {
+      ofstream f{argv[1]};
+      return EXIT_SUCCESS;
+    } else {
+      error_code ec;
+      last_write_time(argv[1], file_time_type::clock::now(), ec);
+      return ec.value();
+    }
   }
   return EXIT_FAILURE;
 }
